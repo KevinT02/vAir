a61663da7cfebabf432138cc5d18356a47e238df
Kevin T <tu02.kevin@gmail.com>
2020-11-26 20:48:59 -0500

    AR Menus

---
+++ AR Construction/Assets/Scripts/ActiveStateToggler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveStateToggler : MonoBehaviour
+{
+
+	public void ToggleActive()
+	{
+		gameObject.SetActive(!gameObject.activeSelf);
+	}
+}
---
+++ AR Construction/Assets/Scripts/TrackerScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
+
+public class TrackerScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public AudioSource audio;
+    public Slider audiovolume;
+    
+    public VideoPlayer video;
+    public Button nxtbtn;
+
+    Slider tracking;
+    bool slide = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tracking = GetComponent<Slider>();
+    }
+
+    public void OnPointerDown(PointerEventData a)
+    {
+        slide = true;
+    }
+
+    public void OnPointerUp(PointerEventData a)
+    {
+        float frame = (float)tracking.value * (float)video.frameCount;
+        video.frame = (long)frame;
+        slide = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!slide)
+        {
+            tracking.value = (float)video.frame / (float)video.frameCount;
+        }
+    }
+
+    public void nextbutton()
+    {
+        float nb = GetComponent<Slider>().value;
+        if (nb > 0.99)
+        {
+            nxtbtn.gameObject.SetActive(true);
+        }
+        else
+            nxtbtn.gameObject.SetActive(false);
+    }
+
+    public void volume()
+    {
+        audio.volume = audiovolume.value;
+    }
+}
